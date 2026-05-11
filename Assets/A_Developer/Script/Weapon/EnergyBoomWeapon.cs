d0dace862b042d5e0ab341e4f1537f07d3415c5c
baodev183 <baodev183@gmail.comm>
2026-05-11 16:56:49 +0700

    Implement TMP

--- Assets/A_Developer/Script/Weapon/EnergyBoomWeapon.cs
+++ Assets/A_Developer/Script/Weapon/EnergyBoomWeapon.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 public class EnergyBoomWeaponBase : WeaponBase
 {
     [Header("Boom Setting")]
-    [SerializeField] private GameObject boomPrefab;
     [SerializeField] private float spawnHeight = 1f;
-    protected override bool CanAttack()
-    {
-        if (!base.CanAttack()) return false;
 
-        return FindTargets().Count > 0;
-    }
-    protected override void SpawnProjectile(Transform target)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void SpawnLightning(List<Transform> targets)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void AuraDamage(List<Transform> targets)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void SpawnMeteor(List<Transform> targets)
+    protected override void SpawnMeteor(List<Transform> targets, Transform holdSpawn)
     {
         foreach (Transform target in targets)
         {
             Vector3 spawnPos = target.position + Vector3.up * spawnHeight;
-
-            Instantiate(boomPrefab, spawnPos, Quaternion.identity);
+            WeaponBase effect = Instantiate(weaponData.weaponPrefab, spawnPos, Quaternion.identity);
+            effect.transform.parent = holdSpawn;
         }
     }
 }
